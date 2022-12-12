7e0ec881f5e3e28e6a7c1953e8658a7f05cc264e
사공지은 <gonii0821@naver.com>
2022-12-12 23:11:14 +0900

    useRef hook with typescript

--- todo-ts/src/components/InputField.tsx
+++ todo-ts/src/components/InputField.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./style.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+// InputField: React.FC<Props>
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a tasks" className="input-box" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        placeholder="Enter a tasks"
+        className="input-box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button className="input-submit" type="submit">
         추가
       </button>
