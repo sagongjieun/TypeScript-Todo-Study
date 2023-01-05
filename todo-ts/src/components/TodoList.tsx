fff8b978c16739d9600ddfab612c8e9bc3dd2d8b
sagongjieun <gonii0821@naver.com>
2023-01-05 20:33:22 +0900

    prepare drag and drop

--- todo-ts/src/components/TodoList.tsx
+++ todo-ts/src/components/TodoList.tsx
@@ -6,14 +6,30 @@ import "./style.css";
 interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  completedTodos: string;
+  setCompletedTodos: string;
 }
 
-const TodoList = ({ todos, setTodos }: Props) => {
+const TodoList = ({ todos, setTodos, completedTodos, setCompletedTodos }: Props) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
-      ))}
+    // <div className="todos"><
+    //   {todos.map((todo) => (
+    //     <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+    //   ))}
+    // </div>
+    <div className="container">
+      <div className="todos">
+        <span className="todos-heading">할 일</span>
+        {todos.map((todo) => (
+          <SingleTodo todo={todo} todos={todos} key={todo.id} setTodos={setTodos} />
+        ))}
+      </div>
+      <div className="todos remove">
+        <span className="todos-heading">한 일</span>
+        {todos.map((todo) => (
+          <SingleTodo todo={todo} todos={todos} key={todo.id} setTodos={setTodos} />
+        ))}
+      </div>
     </div>
   );
 };
